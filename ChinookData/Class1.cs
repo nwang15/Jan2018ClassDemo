530caf7feee68eb20dd2f7e9a8f66b86f404c6cf
nwang15 <nwang15@studentmail.nait.ca>
2018-01-12 17:48:31 -0700

    create new project
    create new solution and project for Jan2018ClassDemo

---
+++ ChinookData/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookData
+{
+    public class Class1
+    {
+    }
+}
